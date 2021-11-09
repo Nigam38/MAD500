3216009546c6bea0a0cf8ed214da52de38480061
Nigam38 <w0752460@myscc.ca>
2021-11-08 20:58:49 -0500

    Assignment 6 done.

--- AngularAssignment1/src/app/content-list/content-list.component.ts
+++ AngularAssignment1/src/app/content-list/content-list.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Content} from "../helper-files/content_interface";
+import {ContentService} from "../services/content.service";
 
 @Component({
   selector: 'app-content-list',
@@ -11,67 +12,17 @@ export class ContentListComponent implements OnInit {
   contentArray: Content[];
   message: string;
 
-  constructor() {
+  // constructor() {
+  // }
+  constructor(private contentService: ContentService) {
   }
 
   ngOnInit(): void {
-    this.contentArray = [{
-      id: 1,
-      author: "William Craig",
-      imageUrl: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1498139456l/435556._SX318_.jpg",
-      type: "History",
-      title: "Enemy at the Gates",
-      body: "he Battle for Stalingrad is a book written by William Craig and published in 1973 by Reader's Digest Press and in 1974 by Penguin Publishing.",
-      tags: []
-    },{
-      id: 2,
-      author: " Brian Michael Bendis",
-      imageUrl: "https://upload.wikimedia.org/wikipedia/en/thumb/b/b5/Avengers_vs._X-Men.jpg/250px-Avengers_vs._X-Men.jpg",
-      type: "Comics",
-      title: "Avengers",
-      body: "Avengers vs. X-Men is a 2012 crossover event that was featured in comic books published by Marvel Comics",
-      tags: ['Avengers','X-Men']
-    }, {
-      id: 3,
-      author: "Nigam Patel",
-      imageUrl: "",
-      type: "",
-      title: "Assignment 2",
-      body: "Using component",
-      tags: []
-    }, {
-      id: 4,
-      author: "John Hersey",
-      imageUrl: "https://images-na.ssl-images-amazon.com/images/I/71RuxECCcAL.__BG0,0,0,0_FMpng_AC_UL600_SR390,600_.jpg",
-      type: "Non-fiction",
-      title: "Hiroshima",
-      body: "",
-      tags: ['Bombing','Nuclear']
-    }, {
-      id: 5,
-      author: "James Clear",
-      imageUrl: "https://images-na.ssl-images-amazon.com/images/I/81iAADNy2NL.jpg",
-      type: "",
-      title: "Atomic Habits",
-      body: "Atomic Habits offers a proven framework for improving-every day",
-      tags: ['Good Habits']
-    },{
-      id: 6,
-      author: "Herodotus",
-      imageUrl: "",
-      type: "History",
-      title: "Histories",
-      body: "",
-      tags: []
-    },{
-      id: 7,
-      author: "Daniel Clowes",
-      imageUrl: "",
-      type: "Comics",
-      title: "Ghost World",
-      body: "Ghost World is a graphic novel by Daniel Clowes.",
-      tags: ['Novel']
-    } ];
+
+    this.contentService.getContentObs().subscribe((content) => {
+      this.contentArray = content;
+    });
+
   }
 
   searchContent(search:String):void{
